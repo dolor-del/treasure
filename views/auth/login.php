250031a4b560fc46256a432b30707ee618ac70dd
dolor-del <saboltasivan@gmail.com>
2023-07-27 17:04:28 +0300

    Initial commit

---
+++ views/auth/login.php
@@ -0,0 +1,71 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap4\ActiveForm $form */
+/** @var app\models\LoginForm $model */
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="leave-comment mr0"><!--leave comment-->
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="site-login">
+				<h1><?= Html::encode($this->title) ?></h1>
+
+				<p>Please fill out the following fields to login:</p>
+
+				<?php $form = ActiveForm::begin([
+					'id' => 'login-form',
+					'layout' => 'horizontal',
+					'fieldConfig' => [
+						'template' => "{label}\n{input}\n{error}",
+						'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+						'inputOptions' => ['class' => 'col-lg-3 form-control'],
+						'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+					],
+				]); ?>
+
+					<?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+
+					<?= $form->field($model, 'password')->passwordInput() ?>
+
+					<?= $form->field($model, 'rememberMe')->checkbox([
+						'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+					]) ?>
+
+					<div class="form-group">
+						<div class="offset-lg-1 col-lg-11">
+							<?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+						</div>
+					</div>
+
+				<?php ActiveForm::end(); ?>
+
+				<div class="offset-lg-1" style="color:#999;">
+					You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
+					To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<script
+					type="text/javascript"
+					src="https://vk.com/js/api/openapi.js?168"
+					charset="windows-1251"
+			></script>
+			<script type="text/javascript">
+				VK.init({ apiId: 51612279 });
+			</script>
+
+			<!-- Put this script tag to the place, where the Login block will be -->
+			<div id="vk_auth"></div>
+			<script type="text/javascript">
+				VK.Widgets.Auth("vk_auth", {authUrl: "/auth/login-vk"});
+			</script>
+		</div>
+	</div>
+</div>
